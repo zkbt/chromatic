54f3f94cc2ce385ceb36db705b1397752912189c
Will Waalkes <wiwa8630@colorado.edu>
2021-06-24 08:58:05 -0600

    Update reader.py

--- chromatic/reader.py
+++ chromatic/reader.py
@@ -1,3 +1,8 @@
 '''tool for reading the Eureka
 output files and making a
 Rainbow out of it'''
+
+'''
+input = Eureka data files
+
+output = astropy table? object? which can be passed into Rainbow '''
