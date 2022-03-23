e7019caac4451c5a7a3b06461d3fb0f1edca0d35
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2022-03-22 20:53:03 -0400

    try to fix conflicting units problem with DN

--- chromatic/units.py
+++ chromatic/units.py
@@ -5,8 +5,14 @@ MJy_sr_sq = u.def_unit("(MJy/sr)^2", (u.MJy / u.sr) ** 2)
 u.add_enabled_units([MJy_sr, MJy_sr_sq])
 
 
-data_number = u.def_unit("DN")
 data_number_per_second = u.def_unit("DN/s")
-data_number_per_second_sq = u.def_unit("(DN/s)^2")
+data_number_per_second_sq = u.def_unit("(DN/s)^2", data_number_per_second ** 2)
+for kludge_DN_unit in [data_number_per_second, data_number_per_second_sq]:
+    try:
+        u.add_enabled_units([kludge_DN_unit])
+    except ValueError:
+        pass
 
-u.add_enabled_units([data_number, data_number_per_second, data_number_per_second_sq])
+# FIXME: some of these feel super kuldgy; how do we make this
+# interact more smoothly with units already defined in astropy
+# (for example, DN should work, but is always flaky for me...)
