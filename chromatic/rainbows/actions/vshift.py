67f31a2169a3bfdd59484954b8ca2348f449cb00
Will-Waalkes <wiwa8630@colorado.edu>
2022-05-14 21:32:07 -0400

    first pass at vshift

---
+++ chromatic/rainbows/actions/vshift.py
@@ -0,0 +1,15 @@
+from ...imports import *
+
+def trim(self, velocity=5.):
+    """
+    Apply a doppler shift to the wavelength array
+
+    Parameters
+    ----------
+    velocity
+        the systemic velocity in question, in km/s
+    """
+    c = 3e5 #speed of light in km/s
+    shifted = self.wave * ( 1 / (1 + v/c) )
+    
+    return shifted
