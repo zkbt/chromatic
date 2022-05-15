a6818688a4b630ac6f9f1efc5c08bfa3761cdb3f
Will-Waalkes <wiwa8630@colorado.edu>
2022-05-15 08:03:49 -0400

    Update shift.py

--- chromatic/rainbows/actions/shift.py
+++ chromatic/rainbows/actions/shift.py
@@ -1,6 +1,6 @@
 from ...imports import *
 
-def trim(self, velocity=5.):
+def trim(self, velocity=5*u.km/u.s):
     """
     Apply a doppler shift to the wavelength array
 
@@ -9,7 +9,7 @@ def trim(self, velocity=5.):
     velocity
         the systemic velocity in question, in km/s
     """
-    c = 3e5 #speed of light in km/s
+    c = 3e5 * u.km / u.s #speed of light in km/s
     shifted = self.wave * ( 1 / (1 + v/c) )
     
     return shifted
