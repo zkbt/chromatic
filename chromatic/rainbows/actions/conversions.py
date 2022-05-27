7311b2e407dc31264aabb31a6191e0fd811bba15
Catriona Murray <cam217@cam.ac.uk>
2022-05-27 11:07:57 -0600

    Added tests and made some small changes suggested in git pull

--- chromatic/rainbows/actions/conversions.py
+++ chromatic/rainbows/actions/conversions.py
@@ -3,25 +3,25 @@ from ...imports import *
 __all__ = ["to_nparray", "to_df"]
 
 
-def to_nparray(self, timeformat='h'):
+def to_nparray(self, timeformat='d'):
     """ Convert Rainbow object to 1D and 2D numpy arrays
     Parameters
     ----------
         self : Rainbow object
             chromatic Rainbow object to convert into array format
         timeformat : str
-            (optional, default='hours')
+            (optional, default='d')
             The time format to use (seconds, minutes, hours, days etc.)
     Returns
     ----------
         rflux : np.array
-            flux (MJy/sr)        [n_wavelengths x n_integrations]
-        rfluxe : np.array
-            flux error (MJy/sr)  [n_wavelengths x n_integrations]
+            flux (MJy/sr)         [n_wavelengths x n_integrations]
+        rfluxu : np.array
+            flux uncertainty      [n_wavelengths x n_integrations]
         rtime : np.array
-            time (BJD_TDB, houra) [n_integrations]
+            time (BJD_TDB, hours) [n_integrations]
         rwavel : np.array
-            wavelength (microns) [n_wavelengths]
+            wavelength (microns)  [n_wavelengths]
     """
     secondformat = ['second', 'seconds', 'sec', 's']
     minuteformat = ['minute', 'minutes', 'min', 'm']
@@ -29,10 +29,10 @@ def to_nparray(self, timeformat='h'):
     dayformat = ['day', 'days', 'd']
     yearformat = ['year', 'years', 'y']
 
-    rflux = np.array(self.fluxlike['flux'])  # flux (MJy/sr)                : [n_wavelengths x n_integrations]
-    rfluxe = np.array(self.fluxlike['uncertainty'])  # flux error (MJy/sr)  : [n_wavelengths x n_integrations]
-    rtime = np.array(self.timelike['time'])  # time (BJD_TDB, hours)        : [n_integrations]
-    rwavel = np.array(self.wavelike['wavelength'])  # wavelength (microns)  : [n_wavelengths]
+    rflux  = np.array(self.fluxlike['flux'])  # flux                         : [n_wavelengths x n_integrations]
+    rfluxu = np.array(self.fluxlike['uncertainty'])  # uncertainty           : [n_wavelengths x n_integrations]
+    rtime  = np.array(self.timelike['time'])  # time (BJD_TDB, hours)        : [n_integrations]
+    rwavel  = np.array(self.wavelike['wavelength'])  # wavelength (microns)  : [n_wavelengths]
 
     # change the time array into the requested format (e.g. seconds, minutes, days etc.)
     if timeformat in secondformat:
@@ -40,9 +40,9 @@ def to_nparray(self, timeformat='h'):
     elif timeformat in minuteformat:
         rtime = rtime * 60
     elif timeformat in hourformat:
-        # hours is the default time setting
         pass
     elif timeformat in dayformat:
+        # hours is the default time setting
         rtime = rtime / 24.
     elif timeformat in yearformat:
         rtime = rtime / (24 * 365.)
@@ -50,29 +50,29 @@ def to_nparray(self, timeformat='h'):
         warnings.warn("Unrecognised Time Format!")
         return
 
-    return rflux, rfluxe, rtime, rwavel
+    return rflux, rfluxu, rtime, rwavel
 
 
-def to_df(self, timeformat='h'):
+def to_df(self, timeformat='d'):
     """ Convert Rainbow object to pandas dataframe
     Parameters
     ----------
         self : Rainbow object
             chromatic Rainbow object to convert into pandas df format
         timeformat : str
-            (optional, default='hours')
+            (optional, default='d')
             The time format to use (seconds, minutes, hours, days etc.)
     Returns
     ----------
         pd.DataFrame
     """
     # extract 1D/2D formats for the flux, uncertainty, time and wavelengths
-    rflux, rfluxe, rtime, rwavel = self.to_nparray(timeformat)
+    rflux, rfluxu, rtime, rwavel = self.to_nparray(timeformat)
 
     # put all arrays onto same dimensions
     x, y = np.meshgrid(rtime, rwavel)
     rainbow_dict = {f"Time ({timeformat})": x.ravel(), "Wavelength (microns)": y.ravel(), "Flux": rflux.ravel(),
-                    "Flux Error": rfluxe.ravel()}
+                    "Flux Uncertainty": rfluxu.ravel()}
 
     # convert to pandas dataframe
     df = pd.DataFrame(rainbow_dict)
---
+++ chromatic/tests/test_conversions.py
@@ -0,0 +1,65 @@
+from ..rainbows import *
+
+
+def test_to_df():
+
+    r = SimulatedRainbow(
+        signal_to_noise=10,
+        dt=1 * u.minute,
+        R=50)
+
+    r_df = r.to_df()
+
+    # ensure the length of the df is the same as the original rainbow
+    assert len(r_df) == r.nflux
+
+    # ensure we have the right column names
+    columnnames = r_df.columns
+    for colname in ['Time (d)', 'Wavelength (microns)', 'Flux', 'Flux Uncertainty']:
+        assert colname in columnnames
+
+    # check the values in the df match the rainbow
+    assert r_df['Time (d)'].values[0] == r.time.to_value()[0] / 24.  # default=days
+    assert r_df['Wavelength (microns)'].values[0] == r.wavelength.to_value()[0]
+    assert r_df['Flux'].values[0] == r.flux[0, 0]
+    assert r_df['Flux Uncertainty'].values[0] == r.uncertainty[0, 0]
+
+    # test the timeformat parameter
+    for timeformat in ['h', 'hour', 'day', 'minute', 'second', 's']:
+        r_df = r.to_df(timeformat=timeformat)
+        assert f'Time ({timeformat})' in r_df.columns
+
+
+def test_to_nparray():
+    r = SimulatedRainbow(
+        signal_to_noise=100,
+        dt=1 * u.minute,
+        R=50)
+
+    rflux, rfluxu, rtime, rwavel = r.to_nparray()
+
+    assert len(rtime) == r.ntime
+    assert len(rwavel) == r.nwave
+    assert len(rtime)*len(rwavel) == r.nflux
+    assert len(rflux.flatten()) == r.nflux
+    assert len(rfluxu.flatten()) == r.nflux
+    assert np.shape(rflux) == r.shape
+    assert np.shape(rfluxu) == r.shape
+
+    assert np.all(rflux == r.flux)
+    assert np.all(rfluxu == r.uncertainty)
+    assert np.all(rwavel == r.wavelength.to_value())
+    assert np.all(rwavel == r.wavelength.to_value())
+
+    assert np.all(rtime == r.time.to_value()/24.)  # the default is days
+
+    # test if the hours format works
+    rflux, rfluxu, rtime, rwavel = r.to_nparray(timeformat='h')
+    assert np.all(rtime == r.time.to_value())
+
+    # test if the minutes format works
+    rflux, rfluxu, rtime, rwavel = r.to_nparray(timeformat='min')
+    assert np.all(rtime == r.time.to_value()*60)
+
+
+
