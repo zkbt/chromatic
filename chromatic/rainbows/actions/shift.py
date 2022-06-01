edaef4e555334f4b74848c1a554c1379eb95f534
Will-Waalkes <wiwa8630@colorado.edu>
2022-06-01 12:18:01 -0600

    Update shift.py

--- chromatic/rainbows/actions/shift.py
+++ chromatic/rainbows/actions/shift.py
@@ -14,7 +14,11 @@ def shift(self, velocity=5*u.km/u.s):
     
     new = self._create_copy()
     
+<<<<<<< Updated upstream
     lightspeed = c.c.to('km/s') #speed of light in km/s
+=======
+    lightspeed = con.c.to('km/s') #speed of light in km/s
+>>>>>>> Stashed changes
     new_wavelength = new.wavelength * ( 1 / (1 - velocity/lightspeed) )
     new.wavelike['wavelength'] = new_wavelength
     
