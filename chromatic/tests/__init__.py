9d4bb08e746792df12c7a14addb986a3013da375
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2022-06-07 18:08:38 -0600

    add fixes to pass tests

--- chromatic/tests/__init__.py
+++ chromatic/tests/__init__.py
@@ -5,7 +5,7 @@ from .test_rainbow_normalize import *
 from .test_rainbow_binning import *
 from .test_rainbow_simulations import *
 from .test_rainbow_visualizations import *
-from .test_rainbow_multi import *
+from .test_multi import *
 from .test_resampling import *
 from .test_units import *
 from .test_align_wavelengths import *
