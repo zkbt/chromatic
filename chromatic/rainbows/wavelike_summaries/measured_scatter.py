e7d9cc32962820623fec25299ac3d027dd081b83
Patcharapol Wachiraphan <patcharapol.wac@gmail.com>
2022-06-13 16:51:23 -0600

    add ability to choose fluxlike quantities to calculate measured_scatter (grad flux for example)

--- chromatic/rainbows/wavelike_summaries/measured_scatter.py
+++ chromatic/rainbows/wavelike_summaries/measured_scatter.py
@@ -3,7 +3,7 @@ from ...imports import *
 __all__ = ["get_measured_scatter"]
 
 
-def get_measured_scatter(self, method="standard-deviation"):
+def get_measured_scatter(self,quantity='flux', method="standard-deviation"):
     """
     Get measured scatter from a chromatic rainbow.
 
@@ -20,13 +20,13 @@ def get_measured_scatter(self, method="standard-deviation"):
 
     if method == "standard-deviation":
         scatters = np.zeros(len(self.wavelength))
-        for i, row in enumerate(self.flux):
+        for i, row in enumerate(self.fluxlike[quantity]):
             scatters[i - 1] = np.std(row)
         return scatters
 
     if method == "MAD":
         scatters = median_abs_deviation(
-            self.flux.copy(), axis=1, scale="normal"
+            self.fluxlike[quantity].copy(), axis=1, scale="normal"
         )  # Setting the scale 'normal' is like * 1.48
         return scatters
 
