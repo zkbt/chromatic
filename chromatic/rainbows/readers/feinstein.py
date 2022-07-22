b0ecca85d8e50f72f31cd1578392f2417f3e5c1c
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2022-07-22 09:15:13 -0600

    add feinstein h5

--- chromatic/rainbows/readers/feinstein.py
+++ chromatic/rainbows/readers/feinstein.py
@@ -1,13 +1,14 @@
 # import the general list of packages
 from ...imports import *
+from h5py import File
 
 # define list of the only things that will show up in imports
-__all__ = ["from_feinstein"]
+__all__ = ["from_feinstein_numpy", "from_feinstein_h5"]
 
 
-def from_feinstein(rainbow, filepath):
+def from_feinstein_numpy(rainbow, filepath):
     """
-    Populate a Rainbow from a file in the feinstein format.
+    Populate a Rainbow from a file in the feinstein numpy format.
 
     Parameters
     ----------
@@ -32,3 +33,30 @@ def from_feinstein(rainbow, filepath):
 
     rainbow.fluxlike["flux"] = spectra.T * 1
     rainbow.fluxlike["uncertainty"] = err.T * 1
+
+
+def from_feinstein_h5(self, filepath, order=1, version="opt"):
+    """
+    Populate a Rainbow from a file in the feinstein H5 format.
+
+    Parameters
+    ----------
+
+    rainbow : Rainbow
+        The object to be populated.
+    filepath : str
+        The path to the file to load.
+    """
+
+    f = File(filepath)
+
+    astropy_times = Time(np.array(f["time"]), format="mjd", scale="tdb")
+    self.set_times_from_astropy(astropy_times, is_barycentric=True)  # ???
+    self.wavelike["wavelength"] = (
+        np.array(f[f"wavelength_order_{order}"]) * u.micron * 1
+    )
+    for k in ["box_flux", "box_var", "opt_flux", "opt_var"]:
+        self.fluxlike[k] = np.transpose(np.array(f[f"{k}_order_{order}"]) * 1)
+
+    self.fluxlike["flux"] = self.fluxlike[f"{version}_flux"] * 1
+    self.fluxlike["uncertainty"] = self.fluxlike[f"{version}_var"] * 1
